a3c8382d9b6a13d2a240df6058fdc1452ec1f47b
Ray Castro <captray108@gmail.com>
2020-11-20 17:44:51 -0800

    feature. tested initial wiring of morse code lib with react app.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import MorseCodeGenerator from 'morse-code/source/morsecodegenerator';
+
 function App() {
+
+  let mcg = new MorseCodeGenerator();
+  let code = mcg.generate("love");
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>{code}</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/raydecastro/morse-code-ui-react"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          morsey!
         </a>
       </header>
     </div>
