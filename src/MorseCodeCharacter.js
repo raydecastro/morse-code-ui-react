0a939d9eea6fc7456bdfea38dc9d0a2190ad0935
Ray Castro <captray108@gmail.com>
2020-11-24 00:46:11 -0800

    cleanup. removed unused constructor

--- src/MorseCodeCharacter.js
+++ src/MorseCodeCharacter.js
@@ -1,10 +1,6 @@
 import { Component } from 'react';
 
 class MorseCodeCharacter extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div className="morseCodeCharacter">
